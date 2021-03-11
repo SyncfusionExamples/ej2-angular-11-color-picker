ae2e7beec2937fb9c195ccce82437d1d324d505a
Saranya Murugasamy <saranya.murugasamy@syncfusion.com>
2021-03-11 09:25:41 +0530

    Angular 11 Color Picker samples are added

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<input ejs-colorpicker type='color' id='colorpicker'/>`
+})
+export class AppComponent {
+  title = 'angular11ColorPicker-app';
+}
